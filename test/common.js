d05ce0c0db8f1c0febe20bb89f0540d1a5b0ab2e
Andrey Sidorov <sidorares@yandex.ru>
2013-04-18 00:05:21 +1000

    use empty password for travis

--- test/common.js
+++ test/common.js
@@ -1,8 +1,8 @@
 module.exports.createConnection = function() {
   return require('../index.js').createConnection({
-   host: process.env.MYSQL_HOST || '127.0.0.1',
-   user: process.env.MYSQL_USER || 'root',
-   password: process.env.MYSQL_PASSWORD || 'test',
+   host: process.env.MYSQL_HOST  || '127.0.0.1',
+   user: process.env.MYSQL_USER  || 'root',
+   password: process.env.CI ? null : 'test',
    database: 'test',
    port: process.env.MYSQL_PORT || 3306
  });
