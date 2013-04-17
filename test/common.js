d78df3b7461c44de74b6615c8fd8f0a3f29fa8b0
Andrey Sidorov <sidorares@yandex.ru>
2013-04-18 00:29:56 +1000

    define hrtime if not present

--- test/common.js
+++ test/common.js
@@ -1,4 +1,13 @@
 module.exports.createConnection = function() {
+  // hrtime polyfill for old node versions:
+  if (!process.hrtime)
+    process.hrtime = function(start) {
+      start = [0, 0] || start;
+      var timestamp = Date.now();
+      var seconds = Math.ceil(timestamp/1000);
+      return [seconds - start[0], (timestamp-seconds*1000)*1000 - start[1]];
+    };
+
   return require('../index.js').createConnection({
    host: process.env.MYSQL_HOST  || '127.0.0.1',
    user: process.env.MYSQL_USER  || 'root',
