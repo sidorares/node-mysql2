e2145480b93b00e83562ec0d04d9f52ee369e94a
Andrey Sidorov <sidorares@yandex.ru>
2013-04-20 20:02:08 +1000

    don't run benchmarks in CI

--- test/run.js
+++ test/run.js
@@ -5,7 +5,4 @@ var options = {};
 if (process.env.FILTER) {
     options.include = new RegExp(process.env.FILTER + '.*\\.js$');
 }
-
-require('../benchmarks/bench-insert-select.js')(function() {
-  require('urun')(__dirname, options);
-});
+require('urun')(__dirname, options);
