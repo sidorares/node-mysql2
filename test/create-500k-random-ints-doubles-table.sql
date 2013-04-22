d5e230025c4d320e9da107dbeb9c04d13e1680fc
Andrey Sidorov <sidorares@yandex.ru>
2013-04-22 16:28:47 +1000

    random numbers test table

---
+++ test/create-500k-random-ints-doubles-table.sql
@@ -0,0 +1,31 @@
+-- from http://sixarm.com/about/mysql-create-random-data-text-strings.html
+
+drop table if exists ints;
+drop procedure if exists randomizer;
+
+CREATE TABLE ints (
+    id int(11) NOT NULL AUTO_INCREMENT,
+    val1 int(11),
+    val2 double
+    PRIMARY KEY (id)
+  );
+
+delimiter $$
+create procedure randomize_nums()
+    begin
+      declare i int Default 0 ;
+      declare random char(20);
+      myloop: loop
+      set random=floor(rand() * 99999)) ;
+      set random1=rand() * 99999 ;
+      insert into `foos` (`id`, `val1`, `val2`) VALUES (i+1,random, random1) ;
+      set i=i+1;
+      if i=500000 then
+        leave myloop;
+  end if;
+    end loop myloop;
+  end $$
+delimiter ;
+
+call randomize_nums;
+
