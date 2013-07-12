b703b0a9a6eb966ea0a24cce699f49a868dbdd30
Andrey Sidorov <sidorares@yandex.ru>
2013-07-12 21:41:01 +1000

    don't encode Buffer parameters as utf8 #17

---
+++ test/integration/connection/test-buffer-params.js
@@ -0,0 +1,28 @@
+var common     = require('../../common');
+var connection = common.createConnection();
+var assert     = require('assert');
+
+var rows = undefined;
+var rows1 = undefined;
+var fields = undefined;
+var fields1 = undefined;
+
+var buf = Buffer([0x80, 0x90, 1, 2, 3, 4, 5, 6, 7, 8, 9, 100, 100, 255, 255]);
+connection.execute('SELECT HEX(?) as buf', [buf], function(err, _rows, _fields) {
+  if (err) throw err;
+  rows = _rows;
+  fields = _fields;
+});
+
+connection.query('SELECT HEX(?) as buf', [buf], function(err, _rows, _fields) {
+  if (err) throw err;
+  rows1 = _rows;
+  fields1 = _fields;
+});
+
+connection.end();
+
+process.on('exit', function() {
+  assert.deepEqual(rows, [{buf: buf.toString('hex').toUpperCase()}]);
+  assert.deepEqual(rows1, [{buf: buf.toString('hex').toUpperCase()}]);
+});
