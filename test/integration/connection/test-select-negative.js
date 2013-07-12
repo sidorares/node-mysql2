8325717d7e45c8f78cded9d0f2343fc65ef47b01
Andrey Sidorov <sidorares@yandex.ru>
2013-07-12 22:09:21 +1000

    parse 8 byte signed int #13

---
+++ test/integration/connection/test-select-negative.js
@@ -0,0 +1,27 @@
+var common     = require('../../common');
+var connection = common.createConnection();
+var assert     = require('assert');
+
+var rows = undefined;
+var rows1 = undefined;
+var fields = undefined;
+var fields1 = undefined;
+
+connection.execute('SELECT -1 v', [], function(err, _rows, _fields) {
+  if (err) throw err;
+  rows = _rows;
+  fields = _fields;
+});
+
+connection.query('SELECT -1 v', function(err, _rows, _fields) {
+  if (err) throw err;
+  rows1 = _rows;
+  fields1 = _fields;
+});
+
+connection.end();
+
+process.on('exit', function() {
+  assert.deepEqual(rows, [{v: -1}]);
+  assert.deepEqual(rows1, [{v: -1}]);
+});
