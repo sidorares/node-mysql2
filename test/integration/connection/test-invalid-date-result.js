9049f9c80ae3fe6c897088e2e8fddddd17deeef1
Andrey Sidorov <sidorares@yandex.ru>
2013-07-14 00:23:29 +1000

    treat 0/0/0/0/0 datetime as JS invalid time (NaN) #16

---
+++ test/integration/connection/test-invalid-date-result.js
@@ -0,0 +1,21 @@
+var common     = require('../../common');
+var connection = common.createConnection();
+var assert     = require('assert');
+
+var rows = undefined;
+
+connection.execute('SELECT TIMESTAMP(0000-00-00) t', [], function(err, _rows, _fields) {
+  if (err) throw err;
+  rows = _rows;
+});
+
+connection.end();
+
+function isInvalidTime(t) {
+  return isNaN(t.getTime());
+}
+
+process.on('exit', function() {
+  assert.deepEqual(Object.prototype.toString.call(rows[0].t), "[object Date]");
+  assert.deepEqual(isInvalidTime(rows[0].t), true);
+});
