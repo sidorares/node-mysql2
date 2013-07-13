07cead78c70151f704b8696c4d90dbc2265566f3
Andrey Sidorov <sidorares@yandex.ru>
2013-07-13 23:56:28 +1000

    send Date parameter in the format mysql expect #14

---
+++ test/integration/connection/test-date-parameter.js
@@ -0,0 +1,17 @@
+var common     = require('../../common');
+var connection = common.createConnection();
+var assert     = require('assert');
+
+var rows = undefined;
+
+connection.execute('SELECT UNIX_TIMESTAMP(?) t', [new Date('1990-01-01 UTC')], function(err, _rows, _fields) {
+  if (err) throw err;
+  rows = _rows;
+  console.log(_rows, _fields);
+});
+
+connection.end();
+
+process.on('exit', function() {
+  assert.deepEqual(rows, [{t: 631152000}]);
+});
