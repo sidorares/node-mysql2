fe2ab0b2703319458455b5443cad4f92678e0cee
Andrey Sidorov <sidorares@yandex.ru>
2013-07-16 09:12:13 +1000

    fix in null bitmap calculation for 8,16,24,.. params #25

---
+++ test/integration/connection/test-execute-null-bitmap.js
@@ -0,0 +1,22 @@
+var common     = require('../../common');
+var connection = common.createConnection();
+var assert     = require('assert');
+
+var params = [1, 2];
+var query = 'select ? + ?';
+
+function dotest() {
+  connection.execute(query + ' as t', params, function(err, _rows, _fields) {
+    assert.equal(err, null);
+    if (params.length < 50) {
+      assert.equal(_rows[0].t, params.reduce(function(x,y) {return x+y;}));
+      query += ' + ?';
+      params.push(params.length);
+      dotest();
+    } else {
+      connection.end();
+    }
+  });
+}
+
+dotest();
