ffc975ce4995fad50bf91b5a3800e3c1c1d8cae0
Andrey Sidorov <sidorares@yandex.ru>
2013-07-15 23:31:51 +1000

    allow execute without parameters #24

--- test/integration/connection/test-execute-1.js
+++ test/integration/connection/test-execute-1.js
@@ -3,17 +3,25 @@ var connection = common.createConnection();
 var assert     = require('assert');
 
 var rows = undefined;
+var rows1 = undefined;
 var fields = undefined;
-connection.execute('SELECT 1+? as test', [5], function(err, _rows, _fields) {
+connection.execute('SELECT 1+? as test', [123], function(err, _rows, _fields) {
   if (err) throw err;
 
   rows = _rows;
+  fields = _fields;
+});
+connection.execute('SELECT 1 as test', function(err, _rows, _fields) {
+  if (err) throw err;
+
+  rows1 = _rows;
   fields = _fields;
 });
 
 connection.end();
 
 process.on('exit', function() {
-  assert.deepEqual(rows, [{'test': 6}]);
+  assert.deepEqual(rows, [{'test': 124}]);
+  assert.deepEqual(rows1, [{'test': 1}]);
   assert.equal(fields[0].name, 'test');
 });
