ae14be4cee7b719f660fa697cda04013bc813c50
Andrey Sidorov <sidorares@yandex.ru>
2013-08-23 17:50:34 +1000

    export createPoolCluster

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var Connection = require('./lib/connection');
 var ConnectionConfig = require('./lib/connection_config');
+
 module.exports.createConnection = function(opts) {
   return new Connection({config: new ConnectionConfig(opts)});
-  //return new Connection(opts);
 };
 
 module.exports.connect = module.exports.createConnection;
@@ -10,13 +10,19 @@ module.exports.connect = module.exports.createConnection;
 module.exports.Connection = Connection;
 module.exports.Types      = require('./lib/constants/types');
 
-var PoolConfig = require('./lib/pool_config');
-var Pool = require('./lib/pool');
 module.exports.createPool = function(config) {
-    return new Pool({config: new PoolConfig(config)});
+  var PoolConfig = require('./lib/pool_config');
+  var Pool       = require('./lib/pool');
+  return new Pool({config: new PoolConfig(config)});
 };
 
-var Server = require('./lib/server');
+exports.createPoolCluster = function(config) {
+  var PoolConfig  = require('./lib/pool_config');
+  var PoolCluster = require('./lib/pool_cluster');
+  return new PoolCluster(config);
+};
+
 module.exports.createServer = function() {
+  var Server = require('./lib/server');
   return new Server();
 };
