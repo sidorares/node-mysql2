969fba6ff1dbf14d53d3efc9f94083b8306cf0b5
Andrey Sidorov <sidorares@yandex.ru>
2013-08-29 20:56:48 +1000

    return NEWDECIMAL and DECIMAL as string, not a buffer even when binary charset #40

---
+++ test/integration/connection/test-execute-newdecimal.js
@@ -0,0 +1,19 @@
+var common     = require('../../common');
+var connection = common.createConnection();
+var assert     = require('assert');
+
+connection.query('CREATE TEMPORARY TABLE t (f DECIMAL(19,4))');
+connection.query('INSERT INTO t VALUES(12345.67)');
+
+var rows, fields;
+connection.execute('SELECT f FROM t', function(err, _rows, _fields) {
+  if (err) throw err;
+  rows = _rows;
+  fields = _fields;
+});
+connection.end();
+
+process.on('exit', function() {
+  assert.deepEqual(rows, [{'f': '12345.6700'}]);
+  assert.equal(fields[0].name, 'f');
+});
