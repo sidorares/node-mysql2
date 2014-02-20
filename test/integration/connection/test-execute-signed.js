40571563ec06388f2e5fad8d215a14cdc3a8ab46
Andrey Sidorov <sidorares@yandex.ru>
2014-02-20 19:02:48 +1100

    roll back incorrect commit (used test as base for example)

--- test/integration/connection/test-execute-signed.js
+++ test/integration/connection/test-execute-signed.js
@@ -2,24 +2,30 @@ var common     = require('../../common');
 var connection = common.createConnection();
 var assert     = require('assert');
 
+var rows = undefined;
+var fields = undefined;
+
 connection.query([
   'CREATE TEMPORARY TABLE `test_table` (',
-  't TINYINT(1)',
+  '`id` int(11) unsigned NOT NULL AUTO_INCREMENT,',
+  '`num` int(15),',
+  '`l` long,',
+  'PRIMARY KEY (`id`)',
   ') ENGINE=InnoDB DEFAULT CHARSET=utf8'
 ].join('\n'));
 
-connection.query('insert into test_table(t) values(?)', [0]);
-connection.query('insert into test_table(t) values(?)', [1]);
-connection.query('insert into test_table(t) values(?)', [-1]);
-connection.query('insert into test_table(t) values(?)', [123]);
+connection.query('insert into test_table(num,l) values(?, 3)', [1]);
+connection.query('insert into test_table(num,l) values(3-?, -10)', [5]);
+connection.query('insert into test_table(num,l) values(4+?, 4000000-?)', [-5, 8000000]);
 
-connection.execute('SELECT * from test_table', function(err, rows, fields) {
+connection.execute('SELECT * from test_table', [], function(err, _rows, _fields) {
   if (err) throw err;
-  console.log(rows);
-
-  rows.forEach(function(r) {
-    console.log(!!r.t ? "true" : "false");
-  });
+  rows = _rows;
+  fields = _fields;
 });
 
 connection.end();
+
+process.on('exit', function() {
+  assert.deepEqual(rows, [{"id":1,"num":1,"l": 3},{"id":2,"num":-2,"l":-10},{"id":3,"num":-1,"l":-4000000}]);
+});
