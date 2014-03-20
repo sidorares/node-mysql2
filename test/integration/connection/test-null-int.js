4c03b23f30949be0608d9543d69243944d79bb4a
Andrey Sidorov <sidorares@yandex.ru>
2014-03-20 12:26:26 +1100

    fix for #85: check for null value in parseFloat

---
+++ test/integration/connection/test-null-int.js
@@ -0,0 +1,21 @@
+var common     = require('../../common');
+var connection = common.createConnection();
+var assert     = require('assert');
+
+var rows;
+
+connection.query('CREATE TEMPORARY TABLE t (i int)');
+connection.query('INSERT INTO t VALUES(null)');
+connection.query('INSERT INTO t VALUES(123)');
+
+connection.query('SELECT * from t', function(err, _rows, _fields) {
+  if (err) throw err;
+  rows = _rows;
+});
+
+connection.end();
+
+process.on('exit', function() {
+  assert.deepEqual(rows[0], {i: null});
+  assert.deepEqual(rows[1], {i: 123});
+});
