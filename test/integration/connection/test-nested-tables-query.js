dda4d7a2af94624a6b33d713d0f0add6d4464e1c
Andrey Sidorov <sidorares@yandex.ru>
2014-04-28 08:11:11 +1000

    nestTables and rowsAsArray in prepared statements

--- test/integration/connection/test-nested-tables-query.js
+++ test/integration/connection/test-nested-tables-query.js
@@ -23,17 +23,46 @@ var options2 = {
   nestTables: '_',
   sql: 'SELECT * FROM ' + table
 };
-var rows1, rows2;
+var options3 = {
+  rowsAsArray: true,
+  sql: 'SELECT * FROM ' + table
+};
+var rows1, rows2, rows3, rows1e, rows2e, rows3e;
 
 connection.query(options1, function(err, _rows) {
   if (err) throw err;
 
   rows1 = _rows;
 });
+
 connection.query(options2, function(err, _rows) {
   if (err) throw err;
 
   rows2 = _rows;
+});
+
+connection.query(options3, function(err, _rows) {
+  if (err) throw err;
+
+  rows3 = _rows;
+});
+
+connection.execute(options1, function(err, _rows) {
+  if (err) throw err;
+
+  rows1e = _rows;
+});
+
+connection.execute(options2, function(err, _rows) {
+  if (err) throw err;
+
+  rows2e = _rows;
+});
+
+connection.execute(options3, function(err, _rows) {
+  if (err) throw err;
+
+  rows3e = _rows;
 });
 
 connection.end();
@@ -42,7 +71,16 @@ process.on('exit', function() {
   assert.equal(rows1.length, 1);
   assert.equal(rows1[0].nested_test.id, 1);
   assert.equal(rows1[0].nested_test.title, 'test');
+  
   assert.equal(rows2.length, 1);
   assert.equal(rows2[0].nested_test_id, 1);
   assert.equal(rows2[0].nested_test_title, 'test');
+
+  assert.equal(Array.isArray(rows3[0]), true);
+  assert.equal(rows3[0][0], 1);
+  assert.equal(rows3[0][1], 'test');
+
+  assert.deepEqual(rows1, rows1e);
+  assert.deepEqual(rows2, rows2e);
+  assert.deepEqual(rows3, rows3e);
 });
