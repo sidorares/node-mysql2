91ab6cf0a90d2414b7d00086c70e03b1831e8ed3
Andrey Sidorov <sidorares@yandex.com>
2014-12-03 15:42:52 +1100

    WIP. initial test

---
+++ test/integration/connection/test-prepare-then-execute.js
@@ -0,0 +1,15 @@
+var common     = require('../../common');
+var connection = common.createConnection();
+var assert     = require('assert');
+
+connection.prepare('select 1 + ? + ? as test', function(err, stmt) {
+  assert(stmt.id > 0);
+  debugger;
+});
+
+
+process.on('exit', function() {
+  //assert.deepEqual(rows, [{'test': 124}]);
+  //assert.deepEqual(rows1, [{'test': 1}]);
+  //assert.equal(fields[0].name, 'test');
+});
