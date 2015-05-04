49206d2fcb693c1d476ae196e14e0b9ee3dab6de
Andrey Sidorov <sidorares@yandex.com>
2015-05-04 12:46:20 +1000

    fix packet serialisation assertion

--- test/unit/packets/test-ok-autoinc.js
+++ test/unit/packets/test-ok-autoinc.js
@@ -4,6 +4,6 @@ var packets    = require('../../../lib/packets/index.js');
 var packet = packets.OK.toPacket({affectedRows: 0, insertId: 1});
 
 // 5 bytes for an OK packet, plus one byte to store affectedRows plus one byte to store the insertId
-assert.equal( packet.length(), 7, 
+assert.equal( packet.length(), 11,
     'OK packets with 0 affectedRows and a minimal insertId should be '+
-    '7 bytes long, got '+packet.length()+' byte(s)' );
+    '11 bytes long, got '+packet.length()+' byte(s)' );
