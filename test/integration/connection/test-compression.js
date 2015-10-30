560ea2f0f9a67b8f0752ace1db26032f7155c96a
Andrey Sidorov <andrey.sidorov@digital-logic.com.au>
2015-10-31 08:08:35 +1100

    add link to docs

--- test/integration/connection/test-compression.js
+++ test/integration/connection/test-compression.js
@@ -2,15 +2,17 @@ var mysql = require('./index.js')
 
 // TODO figure out reliable way to check if compression is enabled
 
+// this does not work for me for some reason - https://dev.mysql.com/doc/refman/5.1/en/server-status-variables.html#statvar_Compression
 
 // select big string, check that diff in Bytes_sent is less than string lingth
 
 // WIP
 //
-  var connection = mysql.createConnection({ user: 'root', password: '', port: 3307, compress: true, debug: true });
+  var connection = mysql.createConnection({ user: 'root', password: '', port: 3306, compress: true, debug: true });
   connection.query("SHOW GLOBAL STATUS where Variable_name like 'Bytes_sent'", function (err, rows, fields) {
-    console.log(err, rows[0].Value);
+    var sent1 = rows[0].Value;
+    process.exit(0);
     connection.query("SHOW GLOBAL STATUS where Variable_name like 'Bytes_sent'", function (err, rows, fields) {
-      console.log(err, rows[0].Value);
+      var sent2 = rows[0].Value;
     })
   })
