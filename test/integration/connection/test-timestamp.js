26caae408a2a184d246beaed436de2264351312a
Andrey Sidorov <andrey.sidorov@digital-logic.com.au>
2016-01-24 11:46:38 +1100

    test 11-byte binary TIMESTAMP

--- test/integration/connection/test-timestamp.js
+++ test/integration/connection/test-timestamp.js
@@ -6,8 +6,16 @@ connection.query('CREATE TEMPORARY TABLE t (f TIMESTAMP)');
 connection.query('INSERT INTO t VALUES(\'0000-00-00 00:00:00\')');
 connection.query('INSERT INTO t VALUES(\'2013-01-22 01:02:03\')');
 
+// test 11-byte timestamp - https://github.com/sidorares/node-mysql2/issues/254
+connection.query('CREATE TEMPORARY TABLE t1 (foo varchar(250) NOT NULL, f timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), bar varchar(250) NOT NULL)')
+connection.query('INSERT INTO t1 (foo, bar) VALUES("foo1", "bar1")');
+connection.query('INSERT INTO t1 (foo, bar) VALUES("foo2", "bar2")');
+
+
 var rows, fields;
 var rows1, fields1;
+var rows2, fields2;
+var rows3, fields3;
 connection.query('SELECT f FROM t', function(err, _rows, _fields) {
   if (err) throw err;
   rows = _rows;
@@ -17,8 +25,21 @@ connection.execute('SELECT f FROM t', function(err, _rows, _fields) {
   if (err) throw err;
   rows1 = _rows;
   fields1 = _fields;
+});
+
+connection.execute('SELECT * FROM t1', function(err, _rows, _fields) {
+  if (err) throw err;
+  rows2 = _rows;
+  fields2 = _fields;
   connection.end();
 });
+
+connection.execute('SELECT * FROM t1', function(err, _rows, _fields) {
+  if (err) throw err;
+  rows3 = _rows;
+  fields3 = _fields;
+  connection.end();
+})
 
 process.on('exit', function() {
   assert.deepEqual(rows[0].f.toString(), 'Invalid Date');
@@ -33,4 +54,8 @@ process.on('exit', function() {
   assert.equal(fields[0].name, 'f');
   assert.deepEqual(rows[1], rows1[1]);
   assert.deepEqual(fields[0].inspect(), fields1[0].inspect());
+
+  assert(rows2[0].f instanceof Date);
+  assert.deepEqual(rows2[1], rows2[1]);
+  assert.deepEqual(fields2[0].inspect(), fields3[0].inspect());
 });
