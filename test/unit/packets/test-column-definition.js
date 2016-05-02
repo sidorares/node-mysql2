0d65b49115eefaf8d9ed06d3221c8c43bfdd05c0
Vadim Ogievetsky <vadimon@gmail.com>
2016-05-02 10:11:14 -0700

    fixed mysqlproxy example

--- test/unit/packets/test-column-definition.js
+++ test/unit/packets/test-column-definition.js
@@ -42,3 +42,23 @@ assert.equal(
   packet.buffer.toString('hex', 4),
   '036465660e735fd0bfd0bed0b3d0bed0b4d0b80e745fd0bfd0bed0b3d0bed0b4d0b80f6f745fd0bfd0bed0b3d0bed0b4d0b80e6e5fd0bfd0bed0b3d0bed0b4d0b80f6f6e5fd0bfd0bed0b3d0bed0b4d0b80c2100f4010000088080010000'
 );
+
+// Spec (from example: https://dev.mysql.com/doc/internals/en/protocoltext-resultset.html)
+var packet = ColumnDefinition.toPacket({
+  catalog: 'def',
+  schema: '',
+  name: '@@version_comment',
+  orgName: '',
+  table: '',
+  orgTable: '',
+
+  characterSet: 0x08, // latin1_swedish_ci
+  columnLength: 0x1c,
+  flags: 0,
+  columnType: 0xfd,
+  decimals: 0x1f
+}, sequenceId);
+assert.equal(
+    packet.buffer.toString('hex', 4),
+    '0364656600000011404076657273696f6e5f636f6d6d656e74000c08001c000000fd00001f0000'
+);
