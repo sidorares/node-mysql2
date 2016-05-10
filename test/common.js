34d9b0603aadbdb02c0f016138ff7742ac1a72da
Andrey Sidorov <sidorares@yandex.com>
2016-05-10 10:27:49 +1000

    set database name

--- test/common.js
+++ test/common.js
@@ -2,7 +2,7 @@ var config = {
   host: process.env.MYSQL_HOST || '127.0.0.1',
   user: process.env.MYSQL_USER || 'root',
   password: process.env.CI ? process.env.MYSQL_PASSWORD : '',
-//  database: process.env.MYSQL_DATABASE || 'test',
+  database: process.env.MYSQL_DATABASE || 'test',
   compress: process.env.MYSQL_USE_COMPRESSION,
   port: process.env.MYSQL_PORT || 3306
 };
