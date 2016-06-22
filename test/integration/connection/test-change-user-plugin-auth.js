4b73868eb1f074de4bc3987ea3d1848d76cb5e45
Andrey Sidorov <andrey.sidorov@digital-logic.com.au>
2016-06-23 08:32:31 +1000

    remove debug

--- test/integration/connection/test-change-user-plugin-auth.js
+++ test/integration/connection/test-change-user-plugin-auth.js
@@ -1,7 +1,6 @@
 var assert = require('assert');
 var common = require('../../common');
 var connection = common.createConnection({
-  debug: true,
   authSwitchHandler: function () {
     throw new Error('should not be called - we expect mysql_native_password '
       + 'plugin switch request to be handled by internal handler');
