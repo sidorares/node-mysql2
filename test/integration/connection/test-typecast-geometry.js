9f1e4467c6f23a23e1ab2092422755329977b6a8
Sushant <sushantdhiman@outlook.com>
2016-07-22 12:39:39 +0530

    use correct method to parse buffers

--- test/integration/connection/test-typecast-geometry.js
+++ test/integration/connection/test-typecast-geometry.js
@@ -15,4 +15,17 @@ connection.query({
   assert.deepEqual(res[0].foo, { x: 11, y: 0 });
 });
 
+connection.query({
+  sql: 'select GeomFromText(\'POINT(11 0)\') as foo',
+  typeCast: function (field, next) {
+    if (field.type === 'GEOMETRY') {
+      return field.buffer();
+    }
+    return next();
+  }
+}, function(err, res) {
+  assert.ifError(err);
+  assert.equal(Buffer.isBuffer(res[0].foo), true);
+});
+
 connection.end();
