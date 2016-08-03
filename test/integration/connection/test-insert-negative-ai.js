2850383c16d44f5ed68023808d5d91ef8d76c820
Sushant <sushantdhiman@outlook.com>
2016-08-03 15:37:54 +0530

    improved negative ai test

--- test/integration/connection/test-insert-negative-ai.js
+++ test/integration/connection/test-insert-negative-ai.js
@@ -2,34 +2,50 @@ var common = require('../../common');
 var connection = common.createConnection();
 var assert = require('assert');
 
-// common.useTestDb(connection);
+var testTable = 'neg-ai-test';
+var testData = 'test negative ai';
 
-var table = 'insert_negative_ai_test';
-var text = ' test test test ';
-connection.query([
-  'CREATE TEMPORARY TABLE `' + table + '` (',
-  '`id` int(11) unsigned NOT NULL AUTO_INCREMENT,',
-  '`title` varchar(255),',
-  'PRIMARY KEY (`id`)',
-  ') ENGINE=InnoDB DEFAULT CHARSET=utf8'
-].join('\n'));
+var selectResult, insertResult;
 
-var result, result2;
-connection.query('INSERT INTO ' + table + ' (id, title) values (-999, "' + text + '")', function (err, _result) {
-  if (err) {
-    throw err;
-  }
-  result = _result;
-  connection.query('SELECT * FROM ' + table + ' WHERE id = ' + result.insertId, function (err, _result2) {
-    result2 = _result2;
-    connection.end();
+var prepareAndTest = function (cb) {
+  connection.query(
+    'CREATE TEMPORARY TABLE `' + testTable + '` (' +
+    '`id` int(11) signed NOT NULL AUTO_INCREMENT,' +
+    '`title` varchar(255),' +
+    'PRIMARY KEY (`id`)' +
+    ') ENGINE=InnoDB DEFAULT CHARSET=utf8', testNegativeAI);
+};
+
+var testNegativeAI = function (err) {
+  assert.ifError(err);
+  // insert the negative AI
+  connection.query(
+    'INSERT INTO `' + testTable + '`' +
+    ' (id, title) values (-999, "' + testData + '")'
+    , function (err, result) {
+
+      assert.ifError(err);
+      insertResult = result;
+
+      // select the row with negative AI
+      connection.query('SELECT * FROM `' + testTable + '`' +
+      ' WHERE id = ' + result.insertId
+      ,function (err, result_) {
+
+        assert.ifError(err);
+        selectResult = result_;
+
+        connection.end();
+      });
   });
-});
+};
+
+prepareAndTest();
 
 process.on('exit', function () {
-  assert.strictEqual(result.insertId, 1);
-  assert.strictEqual(result2.length, 1);
-  // TODO: type conversions
-  assert.equal(result2[0].id, String(result.insertId));
-  assert.equal(result2[0].title, text);
+  assert.strictEqual(insertResult.insertId, -999);
+  assert.strictEqual(selectResult.length, 1);
+
+  assert.equal(selectResult[0].id, String(insertResult.insertId));
+  assert.equal(selectResult[0].title, testData);
 });
