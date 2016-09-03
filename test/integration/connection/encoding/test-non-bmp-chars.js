17d48fba52241b06622b2caca6892ab649dc28ab
Andrey Sidorov <andrey.sidorov@digital-logic.com.au>
2016-09-03 22:05:22 +1000

    add tests for non-BMP chars

---
+++ test/integration/connection/encoding/test-non-bmp-chars.js
@@ -0,0 +1,21 @@
+var mysql = require('../../../../index.js');
+var common = require('../../../common');
+var assert = require('assert');
+
+// 4 bytes in utf8
+var pileOfPoo = '💩';
+
+var connection = common.createConnection({charset: 'UTF8_GENERAL_CI'});
+connection.query('select "💩"', function (err, rows, fields) {
+  assert.equal(fields[0].name, pileOfPoo);
+  assert.equal(rows[0][fields[0].name], pileOfPoo);
+  connection.end();
+});
+
+
+var connection2 = common.createConnection({charset: 'UTF8MB4_GENERAL_CI'});
+connection2.query('select "💩"', function (err, rows, fields) {
+  assert.equal(fields[0].name, '?');
+  assert.equal(rows[0]['?'], pileOfPoo);
+  connection2.end();
+});
