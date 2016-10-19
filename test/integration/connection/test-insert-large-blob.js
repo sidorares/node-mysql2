52046c527046db0ba27acaed474d535daae54ce1
Andrey Sidorov <sidorares@yandex.com>
2016-10-19 12:21:15 +1100

    WIP: fixing compressed protocol support for large packets

--- test/integration/connection/test-insert-large-blob.js
+++ test/integration/connection/test-insert-large-blob.js
@@ -4,9 +4,13 @@ var assert = require('assert');
 var Buffer = require('safe-buffer').Buffer;
 
 var table = 'insert_large_test';
-var content = Buffer.allocUnsafe(19777216); // > 16 megabytes
-content.fill('x', 0, 100000);
-content.fill('o', 100000);
+//var content = Buffer.allocUnsafe(19777216); // > 16 megabytes
+var content = Buffer.allocUnsafe(16); // > 16 megabytes
+//content.fill('x', 0, 100000);
+//content.fill('o', 100000);
+for(var i=0; i<content.length; ++i) {
+  content[i] = Math.floor(Math.random()*256);
+}
 
 var result, result2;
 connection.query('SET GLOBAL max_allowed_packet=56777216', function (err, res) {
@@ -20,7 +24,7 @@ connection.query('SET GLOBAL max_allowed_packet=56777216', function (err, res) {
     'PRIMARY KEY (`id`)',
     ') ENGINE=InnoDB DEFAULT CHARSET=utf8'
   ].join('\n'));
-  connection2.query('INSERT INTO ' + table + ' (content) VALUES(?)', [content], function (err, _result) {
+  connection2.execute('INSERT INTO ' + table + ' (content) VALUES(?)', [content], function (err, _result) {
     assert.ifError(err);
     result = _result;
     connection2.query('SELECT * FROM ' + table + ' WHERE id = ' + result.insertId, function (err, _result2) {
