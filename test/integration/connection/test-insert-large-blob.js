86a89a00bae089d95f3767b2436ba6767514cac3
Andrey Sidorov <sidorares@yandex.com>
2016-10-19 17:36:59 +1100

    WIP big packets in compressed protocol

--- test/integration/connection/test-insert-large-blob.js
+++ test/integration/connection/test-insert-large-blob.js
@@ -5,7 +5,9 @@ var Buffer = require('safe-buffer').Buffer;
 
 var table = 'insert_large_test';
 //var content = Buffer.allocUnsafe(19777216); // > 16 megabytes
-var content = Buffer.allocUnsafe(16); // > 16 megabytes
+var content = Buffer.allocUnsafe(16777216); // > 16 megabytes
+
+//var content = Buffer.allocUnsafe(16); // > 16 megabytes
 //content.fill('x', 0, 100000);
 //content.fill('o', 100000);
 for(var i=0; i<content.length; ++i) {
@@ -24,12 +26,17 @@ connection.query('SET GLOBAL max_allowed_packet=56777216', function (err, res) {
     'PRIMARY KEY (`id`)',
     ') ENGINE=InnoDB DEFAULT CHARSET=utf8'
   ].join('\n'));
-  connection2.execute('INSERT INTO ' + table + ' (content) VALUES(?)', [content], function (err, _result) {
+  //connection2.execute('INSERT INTO ' + table + ' (content) VALUES(?)', [content], function (err, _result) {
+  connection2.query('INSERT INTO ' + table + ' (content) VALUES(?)', [content], function (err, _result) {
     assert.ifError(err);
     result = _result;
     connection2.query('SELECT * FROM ' + table + ' WHERE id = ' + result.insertId, function (err, _result2) {
       result2 = _result2;
-      connection2.end();
+
+      console.log('ending in 3s');
+      setTimeout(() => {
+        connection2.end();
+      }, 3000);
     });
   });
 });
