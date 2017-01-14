c2c2b0869ecd444082dca998aa6db0f37b5f5953
Sushant <sushantdhiman@outlook.com>
2017-01-14 13:34:01 +0530

    test: pool.execute miss release call without args

---
+++ test/integration/regressions/test-#485.js
@@ -0,0 +1,42 @@
+var config = require('../../common.js').config;
+
+var skipTest = false;
+if (typeof Promise == 'undefined') {
+  console.log('no Promise support, skipping test');
+  skipTest = true;
+  return;
+}
+
+var assert = require('assert');
+var createPool = require('../../../promise.js').createPool;
+var PoolConnection = require('../../../lib/pool_connection.js');
+
+// stub
+var release = PoolConnection.prototype.release.bind(PoolConnection.prototype);
+var releaseCalls = 0;
+PoolConnection.prototype.release = function () {
+  releaseCalls++;
+  release.call(this);
+};
+
+function testPoolPromiseExecuteLeak () {
+  var pool = createPool(config);
+  var conn = null;
+  pool
+    .execute('select 1+2 as ttt')
+    .then(function (result) {
+      assert.equal(result[0][0].ttt, 3);
+      return pool.end();
+    })
+    .catch(function (err) {
+      assert.ifError(err);
+    });
+}
+
+testPoolPromiseExecuteLeak();
+
+process.on('exit', function () {
+  if (skipTest) { return; }
+
+  assert.equal(releaseCalls, 1, 'PoolConnection.release was not called');
+});
