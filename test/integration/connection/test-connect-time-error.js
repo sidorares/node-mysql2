1365e955ebb063edf61e8da2d89ab7a73e1e28f0
Andrey Sidorov <sidorares@yandex.com>
2017-07-11 13:51:28 +1000

    add test for #606

---
+++ test/integration/connection/test-connect-time-error.js
@@ -0,0 +1,39 @@
+var mysql = require('../../../index.js');
+var assert = require('assert');
+var Buffer = require('safe-buffer').Buffer;
+
+var server;
+
+const ERROR_TEXT = 'test error';
+
+var portfinder = require('portfinder');
+portfinder.getPort(function(err, port) {
+  var server = mysql.createServer();
+  server.listen(port);
+  server.on('connection', function(conn) {
+    console.log('Here!');
+    conn.writeError(new Error(ERROR_TEXT));
+    conn.close();
+  });
+
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    port: port,
+    user: 'testuser',
+    database: 'testdatabase',
+    password: 'testpassword'
+  });
+
+  connection.query('select 1+1', function(err) {
+    console.log('Here! 1');
+    console.log(err);
+    assert.equal(err.message, ERROR_TEXT);
+  });
+
+  connection.query('select 1+2', function(err) {
+    console.log('Here! 2');
+    assert.equal(err.message, ERROR_TEXT);
+    connection.close();
+    server._server.close();
+  });
+});
