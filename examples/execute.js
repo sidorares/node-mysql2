1ec0ec60a21cdbdd456368218a35dc8e126b4fd1
Ali Servet Donmez <asd@pittle.org>
2018-03-01 11:01:24 +0100

    Fixes execute example
    `execute` function ought to be called on the `connection` rather than `mysql`.

--- examples/execute.js
+++ examples/execute.js
@@ -8,20 +8,20 @@ var connection = mysql.createConnection({
   database: 'test'
 });
 
-mysql.execute('select ?+1 as qqq, ? as rrr, ? as yyy', [1, null, 3], function(
+connection.execute('select ?+1 as qqq, ? as rrr, ? as yyy', [1, null, 3], function(
   err,
   rows,
   fields
 ) {
   console.log(err, rows, fields);
-  mysql.execute('select ?+1 as qqq, ? as rrr, ? as yyy', [3, null, 3], function(
+  connection.execute('select ?+1 as qqq, ? as rrr, ? as yyy', [3, null, 3], function(
     err,
     rows,
     fields
   ) {
     console.log(err, rows, fields);
     mysql.unprepare('select ?+1 as qqq, ? as rrr, ? as yyy');
-    mysql.execute(
+    connection.execute(
       'select ?+1 as qqq, ? as rrr, ? as yyy',
       [3, null, 3],
       function(err, rows, fields) {
