33fa8f5ce6118809d831ea7f54273629472e333f
Joe Reeve <joseph.reeve@googlemail.com>
2018-07-22 14:02:23 +0100

    Update pool-test.js

--- examples/pool-test.js
+++ examples/pool-test.js
@@ -7,10 +7,19 @@ var pool = require('mysql2').createPool({
 
 setInterval(function() {
   for (var i = 0; i < 5; ++i) {
+    pool.query(function(err, db) {
+      console.log(rows, fields);
+      // Connection is automatically released once query resolves
+    });
+  }
+}, 1000);
+
+setInterval(function() {
+  for (var i = 0; i < 5; ++i) {
     pool.getConnection(function(err, db) {
       db.query('select sleep(0.5) as qqq', function(err, rows, fields) {
         console.log(rows, fields);
-        // Connection is automatically released upon query completion
+        db.release()
       });
     });
   }
