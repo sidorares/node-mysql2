175917ea908b53fbc0d6927b11b38aca272769c4
Perlover <perlover@perlover.com>
2020-10-26 19:32:41 +0100

    Added the test for incorrect collation in Mysql cluster 8.0.22 (very strange bug)

---
+++ test/integration/connection/test-execute-bind-ascii.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const common = require('../../common');
+const connection = common.createConnection();
+const assert = require('assert');
+
+const table = 'asciitest';
+
+let rows;
+connection.query(`DROP TABLE IF EXISTS ${table}`);
+connection.query(
+  `CREATE TABLE IF NOT EXISTS ${table} (a char (64) ascii not null, b int unsigned not null) character set utf8`
+);
+connection.query(`INSERT INTO ${table} (a, b) VALUES ('test', 1)`);
+connection.query(`INSERT INTO ${table} (a, b) VALUES ('super', 2)`);
+connection.execute(
+  `SELECT a FROM ${table} WHERE a=? AND b=?`,
+  ['DE', 1],
+  (err) => {
+    if (err) {
+      throw err;
+    }
+  }
+);
+connection.execute(
+  `SELECT a FROM ${table} WHERE a=? AND b=?`,
+  ['test', 1],
+  (err, _rows) => {
+    if (err) {
+      throw err;
+    }
+    rows = _rows;
+    connection.end();
+  }
+);
+
+process.on('exit', () => {
+  assert.deepEqual(rows, [{ a: 'test' }]);
+});
