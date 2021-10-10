52f772712453e2e321780136d036f0a5b80d8d85
Andrey Sidorov <sidorares@yandex.com>
2021-10-10 15:11:32 +1100

    add space

--- test/integration/connection/test-load-infile.js
+++ test/integration/connection/test-load-infile.js
@@ -20,7 +20,7 @@ connection.query(
 const path = './test/fixtures/data.csv';
 const sql =
   `LOAD DATA LOCAL INFILE ? INTO TABLE ${table} ` +
-  `LINES TERMINATED BY '\n'` +
+  `LINES TERMINATED BY '\n' ` +
   `FIELDS TERMINATED BY ? (id, title)`;
 
 let ok;
