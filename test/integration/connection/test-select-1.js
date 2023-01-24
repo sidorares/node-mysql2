703ecb2f788cf32acb1b49c7786ff6845640e215
Andrey Sidorov <andrey.sidorov@gmail.com>
2023-01-24 11:19:29 +1100

    fix: when port is pased as a string convert it to a number (Bun's net.connect does not automatically convert this)

--- test/integration/connection/test-select-1.js
+++ test/integration/connection/test-select-1.js
@@ -1,8 +1,25 @@
 'use strict';
 
+console.log('Hello from bun test', typeof Bun);
+
 const common = require('../../common');
+
+console.log('after import');
+
 const connection = common.createConnection();
-const assert = require('assert');
+connection.on('connect', function(hello) {
+  console.log('connect', hello.serverVersion, hello.protocolVersion);
+})
+console.log('after create connection');
+//const assert = require('assert');
+connection.query('SELECT 1', (err, _rows, _fields) => {
+  console.log('query callback', err, _rows, _fields);
+  connection.end();
+  console.log('after end connection');
+});
+
+
+/*
 
 let rows = undefined;
 let fields = undefined;
@@ -20,3 +37,5 @@ process.on('exit', () => {
   assert.deepEqual(rows, [{ 1: 1 }]);
   assert.equal(fields[0].name, '1');
 });
+
+*/
