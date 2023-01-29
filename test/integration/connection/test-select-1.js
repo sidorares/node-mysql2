a2392e27de64630affb6e3f6af26f5c59e2e95f9
Andrey Sidorov <andrey.sidorov@gmail.com>
2023-01-29 20:17:01 +1100

    feat: cleanup buffer/string conversions in hashing/xor helpers that were failing in Bun

--- test/integration/connection/test-select-1.js
+++ test/integration/connection/test-select-1.js
@@ -1,41 +1,13 @@
 'use strict';
 
-console.log('Hello from bun test', typeof Bun);
-
+const assert = require('assert');
 const common = require('../../common');
-
-console.log('after import');
-
 const connection = common.createConnection();
-connection.on('connect', function(hello) {
-  console.log('connect', hello.serverVersion, hello.protocolVersion);
-})
-console.log('after create connection');
-//const assert = require('assert');
-connection.query('SELECT 1', (err, _rows, _fields) => {
-  console.log('query callback', err, _rows, _fields);
-  connection.end();
-  console.log('after end connection');
-});
 
-
-/*
-
-let rows = undefined;
-let fields = undefined;
-connection.query('SELECT 1', (err, _rows, _fields) => {
-  if (err) {
-    throw err;
-  }
-
-  rows = _rows;
-  fields = _fields;
-  connection.end();
-});
-
-process.on('exit', () => {
+connection.query('SELECT 1', (err, rows, fields) => {
+  console.log('query callback', err, rows, fields);
+  assert.ifError(err);
   assert.deepEqual(rows, [{ 1: 1 }]);
   assert.equal(fields[0].name, '1');
+  connection.end();
 });
-
-*/
