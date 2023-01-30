a4143fa6b318d396220ca9d9ce29c5c5d6108d54
Andrey Sidorov <andrey.sidorov@gmail.com>
2023-01-30 17:20:48 +1100

    debug

--- test/integration/connection/test-select-1.js
+++ test/integration/connection/test-select-1.js
@@ -5,7 +5,6 @@ const common = require('../../common');
 const connection = common.createConnection();
 
 connection.query('SELECT 1', (err, rows, fields) => {
-  console.log('query callback', err, rows, fields);
   assert.ifError(err);
   assert.deepEqual(rows, [{ 1: 1 }]);
   assert.equal(fields[0].name, '1');
