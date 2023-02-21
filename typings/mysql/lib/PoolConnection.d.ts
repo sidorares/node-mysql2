917888b0f9bc625e65e42c112d6d4f13c616209b
Marlon <marlonfsolisest@gmail.com>
2023-02-20 19:18:57 -0500

    fix(Pool): create PoolConnectionBase.d.ts interface to fix a typescript error.

--- typings/mysql/lib/PoolConnection.d.ts
+++ typings/mysql/lib/PoolConnection.d.ts
@@ -1,7 +1,8 @@
 
 import Connection = require('./Connection');
+import PoolConnectionBase = require('./PoolConnectionBase');
 
-declare class PoolConnection extends Connection {
+declare class PoolConnection extends Connection implements PoolConnectionBase {
     connection: Connection;
     release(): void;
 }
