86655ec6ad8ab8deae11a3c4919ae2ee553f4120
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2023-06-24 03:27:45 -0300

    fix: missing keys for `Types` constant

--- typings/mysql/lib/constants/Types.d.ts
+++ typings/mysql/lib/constants/Types.d.ts
@@ -1,38 +1,68 @@
+interface Types {
+  0x00: string;
+  0x01: string;
+  0x02: string;
+  0x03: string;
+  0x04: string;
+  0x05: string;
+  0x06: string;
+  0x07: string;
+  0x08: string;
+  0x09: string;
+  0x0a: string;
+  0x0b: string;
+  0x0c: string;
+  0x0d: string;
+  0x0e: string;
+  0x0f: string;
+  0x10: string;
+  0xf5: string;
+  0xf6: string;
+  0xf7: string;
+  0xf8: string;
+  0xf9: string;
+  0xfa: string;
+  0xfb: string;
+  0xfc: string;
+  0xfd: string;
+  0xfe: string;
+  0xff: string;
+
+  DECIMAL: number;
+  TINY: number;
+  SHORT: number;
+  LONG: number;
+  FLOAT: number;
+  DOUBLE: number;
+  NULL: number;
+  TIMESTAMP: number;
+  LONGLONG: number;
+  INT24: number;
+  DATE: number;
+  TIME: number;
+  DATETIME: number;
+  YEAR: number;
+  NEWDATE: number;
+  VARCHAR: number;
+  BIT: number;
+  JSON: number;
+  NEWDECIMAL: number;
+  ENUM: number;
+  SET: number;
+  TINY_BLOB: number;
+  MEDIUM_BLOB: number;
+  LONG_BLOB: number;
+  BLOB: number;
+  VAR_STRING: number;
+  STRING: number;
+  GEOMETRY: number;
+}
+
 /**
  * Constant `Types`.
  *
  * Please note that `Types` can only be accessed from the `mysql` object and not imported directly.
  */
-declare const Types: {
-  [key: number]: string;
-  DECIMAL: 0x00;
-  TINY: 0x01;
-  SHORT: 0x02;
-  LONG: 0x03;
-  FLOAT: 0x04;
-  DOUBLE: 0x05;
-  NULL: 0x06;
-  TIMESTAMP: 0x07;
-  LONGLONG: 0x08;
-  INT24: 0x09;
-  DATE: 0x0a;
-  TIME: 0x0b;
-  DATETIME: 0x0c;
-  YEAR: 0x0d;
-  NEWDATE: 0x0e;
-  VARCHAR: 0x0f;
-  BIT: 0x10;
-  JSON: 0xf5;
-  NEWDECIMAL: 0xf6;
-  ENUM: 0xf7;
-  SET: 0xf8;
-  TINY_BLOB: 0xf9;
-  MEDIUM_BLOB: 0xfa;
-  LONG_BLOB: 0xfb;
-  BLOB: 0xfc;
-  VAR_STRING: 0xfd;
-  STRING: 0xfe;
-  GEOMETRY: 0xff;
-};
+declare const Types: Types;
 
 export { Types };
