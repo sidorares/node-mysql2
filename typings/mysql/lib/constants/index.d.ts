609229a973031615cb93b5678b5932cf3714480f
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2023-06-24 03:43:29 -0300

    fix: missing typings for `CharsetToEncoding` constants

--- typings/mysql/lib/constants/index.d.ts
+++ typings/mysql/lib/constants/index.d.ts
@@ -1,4 +1,5 @@
 import { Types } from './Types.js';
 import { Charsets } from './Charsets.js';
+import { CharsetToEncoding } from './CharsetToEncoding.js';
 
-export { Types, Charsets };
+export { Types, Charsets, CharsetToEncoding };
