04eeb6aef4abfd90916b35c68bf5564f91f9e0af
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2023-06-24 02:02:29 -0300

    ci: add tests for `Types` constants

---
+++ test/tsc-build/mysql/constants/Types.ts
@@ -0,0 +1,7 @@
+import { mysql } from '../../index.js';
+
+const BLOB: number = mysql.Types.BLOB;
+const DECIMAL: string = mysql.Types[0x00];
+const DOUBLE: string = mysql.Types[5];
+
+console.log(BLOB, DECIMAL, DOUBLE);
