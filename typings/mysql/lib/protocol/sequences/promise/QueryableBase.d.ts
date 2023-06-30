0f31a41dcfe65d2953447c7f1a8b5c892f2ceed9
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2023-06-30 10:38:24 -0300

    fix: add `ProcedureCallPacket` to promise-based `query` overloads

--- typings/mysql/lib/protocol/sequences/promise/QueryableBase.d.ts
+++ typings/mysql/lib/protocol/sequences/promise/QueryableBase.d.ts
@@ -3,6 +3,7 @@ import {
   FieldPacket,
   RowDataPacket,
   ResultSetHeader,
+  ProcedureCallPacket,
 } from '../../packets/index.js';
 import { QueryOptions, QueryableConstructor } from '../Query.js';
 
@@ -17,40 +18,68 @@ export declare function QueryableBase<T extends QueryableConstructor>(
         | RowDataPacket[]
         | RowDataPacket[][]
         | OkPacket[]
+        | ProcedureCallPacket<
+            | OkPacket
+            | ResultSetHeader
+            | RowDataPacket[]
+            | RowDataPacket[][]
+            | OkPacket[]
+          >
     >(
       sql: string
     ): Promise<[T, FieldPacket[]]>;
     query<
-      T_1 extends
+      T extends
         | OkPacket
         | ResultSetHeader
         | RowDataPacket[]
         | RowDataPacket[][]
         | OkPacket[]
+        | ProcedureCallPacket<
+            | OkPacket
+            | ResultSetHeader
+            | RowDataPacket[]
+            | RowDataPacket[][]
+            | OkPacket[]
+          >
     >(
       sql: string,
       values: any
-    ): Promise<[T_1, FieldPacket[]]>;
+    ): Promise<[T, FieldPacket[]]>;
     query<
-      T_2 extends
+      T extends
         | OkPacket
         | ResultSetHeader
         | RowDataPacket[]
         | RowDataPacket[][]
         | OkPacket[]
+        | ProcedureCallPacket<
+            | OkPacket
+            | ResultSetHeader
+            | RowDataPacket[]
+            | RowDataPacket[][]
+            | OkPacket[]
+          >
     >(
       options: QueryOptions
-    ): Promise<[T_2, FieldPacket[]]>;
+    ): Promise<[T, FieldPacket[]]>;
     query<
-      T_3 extends
+      T extends
         | OkPacket
         | ResultSetHeader
         | RowDataPacket[]
         | RowDataPacket[][]
         | OkPacket[]
+        | ProcedureCallPacket<
+            | OkPacket
+            | ResultSetHeader
+            | RowDataPacket[]
+            | RowDataPacket[][]
+            | OkPacket[]
+          >
     >(
       options: QueryOptions,
       values: any
-    ): Promise<[T_3, FieldPacket[]]>;
+    ): Promise<[T, FieldPacket[]]>;
   };
 } & T;
