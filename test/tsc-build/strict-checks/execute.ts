4dd11c8a0f8d94d1e7c6b64cefda342d1e6dc7be
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2023-07-04 08:59:15 -0300

    ci: add `ProcedureCallPacket` tests for `execute`

--- test/tsc-build/strict-checks/execute.ts
+++ test/tsc-build/strict-checks/execute.ts
@@ -49,6 +49,48 @@ import { access, sql } from '../promise/baseConnection.js';
 
     console.log(err, result, fields);
   });
+
+  conn.execute<mysql.ProcedureCallPacket>(sql, (_e, _r, _f) => {
+    const err: mysql.QueryError | null = _e;
+    const result: mysql.ProcedureCallPacket = _r;
+    const fields: mysql.FieldPacket[] = _f;
+
+    console.log(err, result, fields);
+  });
+
+  conn.execute<mysql.ProcedureCallPacket<mysql.RowDataPacket[]>>(
+    sql,
+    (_e, _r, _f) => {
+      const err: mysql.QueryError | null = _e;
+      const result: [...mysqlp.RowDataPacket[], mysql.ResultSetHeader] = _r;
+      const fields: mysql.FieldPacket[] = _f;
+
+      console.log(err, result, fields);
+    }
+  );
+
+  conn.execute<mysql.ProcedureCallPacket<mysql.RowDataPacket[][]>>(
+    sql,
+    (_e, _r, _f) => {
+      const err: mysql.QueryError | null = _e;
+      const result: [...mysqlp.RowDataPacket[][], mysql.ResultSetHeader] = _r;
+      const fields: mysql.FieldPacket[] = _f;
+
+      console.log(err, result, fields);
+    }
+  );
+
+  conn.execute<
+    mysql.ProcedureCallPacket<
+      mysql.OkPacket | mysql.OkPacket[] | mysql.ResultSetHeader
+    >
+  >(sql, (_e, _r, _f) => {
+    const err: mysql.QueryError | null = _e;
+    const result: mysql.ResultSetHeader = _r;
+    const fields: mysql.FieldPacket[] = _f;
+
+    console.log(err, result, fields);
+  });
 }
 
 // Promise
@@ -89,4 +131,42 @@ import { access, sql } from '../promise/baseConnection.js';
 
     console.log(result, fields);
   });
+
+  conn.execute<mysqlp.ProcedureCallPacket>(sql).then(([_r, _f]) => {
+    const result: mysqlp.ProcedureCallPacket = _r;
+    const fields: mysqlp.FieldPacket[] = _f;
+
+    console.log(result, fields);
+  });
+
+  conn
+    .execute<mysqlp.ProcedureCallPacket<mysqlp.RowDataPacket[]>>(sql)
+    .then(([_r, _f]) => {
+      const result: [...mysqlp.RowDataPacket[], mysql.ResultSetHeader] = _r;
+      const fields: mysqlp.FieldPacket[] = _f;
+
+      console.log(result, fields);
+    });
+
+  conn
+    .execute<mysqlp.ProcedureCallPacket<mysqlp.RowDataPacket[][]>>(sql)
+    .then(([_r, _f]) => {
+      const result: [...mysqlp.RowDataPacket[][], mysql.ResultSetHeader] = _r;
+      const fields: mysqlp.FieldPacket[] = _f;
+
+      console.log(result, fields);
+    });
+
+  conn
+    .execute<
+      mysqlp.ProcedureCallPacket<
+        mysqlp.OkPacket | mysqlp.OkPacket[] | mysqlp.ResultSetHeader
+      >
+    >(sql)
+    .then(([_r, _f]) => {
+      const result: mysqlp.ResultSetHeader = _r;
+      const fields: mysqlp.FieldPacket[] = _f;
+
+      console.log(result, fields);
+    });
 })();
