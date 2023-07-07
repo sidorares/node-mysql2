47c2b5b695bec9a3bb1ae49a1f89eb9727909a58
Andrey Sidorov <andrey.sidorov@gmail.com>
2023-07-07 14:41:29 +1000

    lint

--- test/integration/connection/test-select-ssl.js
+++ test/integration/connection/test-select-ssl.js
@@ -4,12 +4,12 @@ const assert = require('assert');
 const common = require('../../common');
 const connection = common.createConnection();
 
-connection.query(`SHOW STATUS LIKE 'Ssl_cipher'`, (err, rows, fields) => {
+connection.query(`SHOW STATUS LIKE 'Ssl_cipher'`, (err, rows) => {
   assert.ifError(err);
   if (process.env.MYSQL_USE_TLS === '1') {
     assert(rows[0].Value.startsWith('TLS_AES_'));
   } else {
-     assert.deepEqual(rows, [{ Variable_name: 'Ssl_cipher', Value: '' }]);
+    assert.deepEqual(rows, [{ Variable_name: 'Ssl_cipher', Value: '' }]);
   }
   connection.end();
 });
