28ad73cab7593a10a1ea36357a3480ad58792073
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2023-07-08 14:14:47 -0300

    docs: add `RowDataPacket[]` basic example

---
+++ examples/typescript/row-data-packet.ts
@@ -0,0 +1,76 @@
+/**
+ * docker run --rm --name mysql2-examples -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=mysql2_examples -p 3306:3306 -d mysql:latest
+ *
+ * The types are explicity for learning purpose
+ *
+ * By extending the `RowDataPacket`, you can use your Interface in `query` and `execute`
+ */
+
+import mysql, {
+  ConnectionOptions,
+  ResultSetHeader,
+  RowDataPacket,
+} from 'mysql2/promise';
+
+interface User extends RowDataPacket {
+  id: number;
+  name: string;
+}
+
+(async () => {
+  const access: ConnectionOptions = {
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    port: 3306,
+    database: 'mysql2_examples',
+  };
+
+  const conn = await mysql.createConnection(access);
+
+  /** Deleting the `users` table, if it exists */
+  await conn.query<ResultSetHeader>('DROP TABLE IF EXISTS `users`;');
+
+  /** Creating a minimal user table */
+  await conn.query<ResultSetHeader>(
+    'CREATE TABLE `users` (`id` INT(11) AUTO_INCREMENT, `name` VARCHAR(50), PRIMARY KEY (`id`));',
+  );
+
+  /** Inserting some users */
+  const [inserted] = await conn.execute<ResultSetHeader>(
+    'INSERT INTO `users`(`name`) VALUES(?), (?), (?), (?);',
+    ['Josh', 'John', 'Marie', 'Gween'],
+  );
+
+  console.log('Inserted:', inserted.affectedRows);
+
+  /** Getting users */
+  const [users] = await conn.query<User[]>(
+    'SELECT * FROM `users` ORDER BY `name` ASC;',
+  );
+
+  users.forEach((user) => {
+    console.log('-----------');
+    console.log('id:  ', user.id);
+    console.log('name:', user.name);
+  });
+
+  await conn.end();
+})();
+
+/** Output
+ *
+ * Inserted: 4
+ * -----------
+ * id:   4
+ * name: Gween
+ * -----------
+ * id:   2
+ * name: John
+ * -----------
+ * id:   1
+ * name: Josh
+ * -----------
+ * id:   3
+ * name: Marie
+ */
