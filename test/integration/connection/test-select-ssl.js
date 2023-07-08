c4e45b8b5ba79345143a8cbcc4efd243061643ce
Andrey Sidorov <andrey.sidorov@gmail.com>
2023-07-08 23:38:50 +1000

    debug Ssl_cipher assertion

--- test/integration/connection/test-select-ssl.js
+++ test/integration/connection/test-select-ssl.js
@@ -7,7 +7,7 @@ const connection = common.createConnection();
 connection.query(`SHOW STATUS LIKE 'Ssl_cipher'`, (err, rows) => {
   assert.ifError(err);
   if (process.env.MYSQL_USE_TLS === '1') {
-    assert(rows[0].Value.startsWith('TLS_AES_'));
+    assert.equal(rows[0].Value.slice(0, 8), 'TLS_AES_');
   } else {
     assert.deepEqual(rows, [{ Variable_name: 'Ssl_cipher', Value: '' }]);
   }
