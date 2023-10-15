a52574feef0446e6b943b8bd4a992dc74061f16e
bim <i.m.baranov@gmail.com>
2023-10-15 15:50:14 +0300

    remove stream.emit('close') on stream end

--- test/integration/connection/test-stream.js
+++ test/integration/connection/test-stream.js
@@ -3,6 +3,7 @@
 const common = require('../../common');
 const connection = common.createConnection();
 const assert = require('assert');
+const { finished } = require("stream/promises");
 
 let rows;
 const rows1 = [];
@@ -62,8 +63,31 @@ connection.execute('SELECT * FROM announcements', (err, _rows) => {
   });
 });
 
+
+const rowsStreamForAwait = []
+async function testStreamForAwait() {
+  const stream = connection.execute('SELECT * FROM announcements').stream()
+  for await (const row of stream) {
+    rowsStreamForAwait.push(row)
+  }
+}
+testStreamForAwait().then()
+
+
+const rowsStreamFinished = []
+async function testStreamFinished() {
+  const stream = connection.execute('SELECT * FROM announcements').stream()
+  stream.on('result', row => {
+    rowsStreamFinished.push(row)
+  })
+  await finished(stream)
+}
+testStreamFinished().then()
+
 process.on('exit', () => {
   assert.deepEqual(rows.length, 2);
   assert.deepEqual(rows, rows1);
   assert.deepEqual(rows, rows2);
+  assert.deepEqual(rows, rowsStreamForAwait);
+  assert.deepEqual(rows, rowsStreamFinished);
 });
