cf3f80ec6f473a8ea17da5776205fb8094169166
yutak23 <yutapilafu@gmail.com>
2023-11-17 16:13:27 +0900

    fix: Malformed FieldPacket

--- typings/mysql/lib/protocol/packets/FieldPacket.d.ts
+++ typings/mysql/lib/protocol/packets/FieldPacket.d.ts
@@ -3,19 +3,18 @@ declare interface FieldPacket {
     name: 'FieldPacket';
   };
   catalog: string;
-  charsetNr: number;
-  db: string;
+  schema: string;
+  characterSet: number;
   decimals: number;
-  default: any;
-  flags: number;
-  length: number;
+  flags: string[];
   name: string;
   orgName: string;
   orgTable: string;
-  protocol41: boolean;
   table: string;
   type: number;
-  zerofill: boolean;
+  typeName: string;
+  encoding: string;
+  columnLength: number;
 }
 
 export { FieldPacket };
