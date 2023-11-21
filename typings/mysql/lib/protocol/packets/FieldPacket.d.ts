8831e092024f8d26fe9272adec8e1a5f115735aa
yutak23 <79501292+yutak23@users.noreply.github.com>
2023-11-21 11:28:11 +0900

    fix: malformed FieldPacket (#2280)
    * fix: Malformed FieldPacket
    
    * fix: change to optional typeo
    
    * fix: add columnType
    
    * fix: type props to optional

--- typings/mysql/lib/protocol/packets/FieldPacket.d.ts
+++ typings/mysql/lib/protocol/packets/FieldPacket.d.ts
@@ -3,19 +3,25 @@ declare interface FieldPacket {
     name: 'FieldPacket';
   };
   catalog: string;
-  charsetNr: number;
-  db: string;
+  charsetNr?: number;
+  db?: string;
+  schema?: string;
+  characterSet?: number;
   decimals: number;
-  default: any;
-  flags: number;
-  length: number;
+  default?: any;
+  flags: number | string[];
+  length?: number;
   name: string;
   orgName: string;
   orgTable: string;
-  protocol41: boolean;
+  protocol41?: boolean;
   table: string;
-  type: number;
-  zerofill: boolean;
+  type?: number;
+  columnType?: number
+  zerofill?: boolean;
+  typeName?: string;
+  encoding?: string;
+  columnLength?: number;
 }
 
 export { FieldPacket };
