2bfcc73da9c453aac19d6177563c100c1c275804
sidorares <sidorares@users.noreply.github.com>
2023-12-20 12:46:36 +0000

    deploy: 15f1669314e7035bfee945cd476d44e24b0e62ea

---
+++ zh-CN/assets/js/1df93b7f.8f7e93f0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[3237],{9754:(e,s,t)=>{t.r(s),t.d(s,{default:()=>r});var n=t(6550),c=t(2263),o=t(5893);const r=function(){const{i18n:e,siteConfig:s}=(0,c.Z)(),{baseUrl:t}=s,r=e.currentLocale,u="en"===r?`${t}docs`:`${t}${r}/docs`;return(0,o.jsx)(n.l_,{to:u})}}}]);
