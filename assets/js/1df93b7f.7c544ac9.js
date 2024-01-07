d5f5ef3ace5ffdba10e4cece10505aded8336fe6
wellwelwel <wellwelwel@users.noreply.github.com>
2024-01-07 23:05:35 +0000

    deploy: c0d77c02d2f4ad22b46a712d270fc2654d26de4e

---
+++ assets/js/1df93b7f.7c544ac9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[3237],{9754:(e,s,n)=>{n.r(s),n.d(s,{default:()=>i});var t=n(6550),c=n(2263),u=n(5893);const i=function(){const{i18n:e,siteConfig:s}=(0,c.Z)(),{baseUrl:n}=s,i=e.currentLocale,o="en"===i||n.includes(i)?`${n}docs`:`${n}${i}/docs`;return(0,u.jsx)(t.l_,{to:o})}}}]);
