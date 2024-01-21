e5e8aa966ef0e6c357a0995f57872ea8fe898e38
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2024-01-20 21:36:40 -0300

    ci(bun): fixes the process exit in Bun Canary tests

--- test/integration/connection/test-select-1.js
+++ test/integration/connection/test-select-1.js
@@ -8,5 +8,9 @@ connection.query('SELECT 1 as result', (err, rows, fields) => {
   assert.ifError(err);
   assert.deepEqual(rows, [{ result: 1 }]);
   assert.equal(fields[0].name, 'result');
-  connection.end();
+
+  connection.end(err => {
+    assert.ifError(err);
+    process.exit(0);
+  });
 });
