de62402e6bf05f5b918f9c4e9225458815883e7e
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2024-01-23 23:35:41 -0300

    ci: compatibility with MySQL 5.7 in typeCast tests

--- test/integration/connection/test-typecast.js
+++ test/integration/connection/test-typecast.js
@@ -7,15 +7,15 @@ const assert = require('assert');
 connection.query('CREATE TEMPORARY TABLE json_test (json_test JSON)');
 connection.query(
   'INSERT INTO json_test VALUES (?)',
-  JSON.stringify({ test: 42 })
+  JSON.stringify({ test: 42 }),
 );
 
 connection.query(
-  'CREATE TEMPORARY TABLE geom_test (p POINT SRID 0, g GEOMETRY NOT NULL SRID 0)'
+  'CREATE TEMPORARY TABLE geom_test (p POINT, g GEOMETRY NOT NULL)',
 );
 connection.query(
   'INSERT INTO geom_test VALUES (ST_GeomFromText("POINT(1 1)"), ' +
-    'ST_GeomFromText("LINESTRING(-71.160281 42.258729,-71.160837 42.259113,-71.161144 42.25932)"))'
+    'ST_GeomFromText("LINESTRING(-71.160281 42.258729,-71.160837 42.259113,-71.161144 42.25932)"))',
 );
 
 connection.query(
@@ -32,7 +32,7 @@ connection.query(
   (err, res) => {
     assert.ifError(err);
     assert.equal(res[0].foo, 'FOO UPPERCASE');
-  }
+  },
 );
 
 connection.query(
@@ -44,7 +44,7 @@ connection.query(
     assert.ifError(err);
     assert(Buffer.isBuffer(res[0].foo));
     assert.equal(res[0].foo.toString('utf8'), 'foobar');
-  }
+  },
 );
 
 connection.query(
@@ -58,7 +58,7 @@ connection.query(
     assert.ifError(err);
     assert.equal(_rows[0].test, null);
     assert.equal(_rows[0].value, 6);
-  }
+  },
 );
 
 connection.query(
@@ -71,7 +71,7 @@ connection.query(
   (err, _rows) => {
     assert.ifError(err);
     assert.equal(_rows[0].json_test.test, 42);
-  }
+  },
 );
 
 connection.execute(
@@ -84,7 +84,7 @@ connection.execute(
   (err, _rows) => {
     assert.ifError(err);
     assert.equal(_rows[0].json_test.test, 42);
-  }
+  },
 );
 
 // read geo fields
@@ -101,16 +101,16 @@ connection.query(
         { x: -71.160837, y: 42.259113 },
         { x: -71.161144, y: 42.25932 },
       ],
-      res[0].g
+      res[0].g,
     );
-  }
+  },
 );
 
 connection.query(
   {
     sql: 'select * from geom_test',
     typeCast: function (field, next) {
-      assert.equal("geom_test", field.table);
+      assert.equal('geom_test', field.table);
 
       if (field.name === 'p' && field.type === 'GEOMETRY') {
         assert.deepEqual({ x: 1, y: 1 }, field.geometry());
@@ -124,7 +124,7 @@ connection.query(
             { x: -71.160837, y: 42.259113 },
             { x: -71.161144, y: 42.25932 },
           ],
-          field.geometry()
+          field.geometry(),
         );
 
         return [
@@ -148,9 +148,9 @@ connection.query(
         { x: -60, y: 50 },
         { x: -50, y: 60 },
       ],
-      res[0].g
+      res[0].g,
     );
-  }
+  },
 );
 
 connection.end();
