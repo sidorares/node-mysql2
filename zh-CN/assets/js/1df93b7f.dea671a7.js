fc03867e22171036e2e9be5583a04c01e8443e2b
wellwelwel <wellwelwel@users.noreply.github.com>
2024-02-20 08:29:13 +0000

    deploy: a7aecaa9090b5430b052cf2f8cb38a594e32b3a7

---
+++ zh-CN/assets/js/1df93b7f.dea671a7.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[4583],{6866:(e,s,n)=>{n.r(s),n.d(s,{default:()=>u});var t=n(6347),c=n(4586),r=n(4848);const u=function(){const{i18n:e,siteConfig:s}=(0,c.A)(),{baseUrl:n}=s,u=e.currentLocale,i="en"===u||n.includes(u)?`${n}docs`:`${n}${u}/docs`;return(0,r.jsx)(t.rd,{to:i})}}}]);
