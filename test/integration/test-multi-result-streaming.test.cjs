a20d122630d1c683e91c27a1f7685f717f9803fc
Davyd McColl <davydm@gmail.com>
2024-03-14 09:06:00 +0200

    :rotating_light: run prettier -w on test-multi-result-streaming.test.cjs

--- test/integration/test-multi-result-streaming.test.cjs
+++ test/integration/test-multi-result-streaming.test.cjs
@@ -4,19 +4,16 @@ const { createConnection } = require('../common.test.cjs');
 (async () => {
   'use strict';
 
-  const
-    conn = createConnection({ multipleStatements: true }),
+  const conn = createConnection({ multipleStatements: true }),
     captured1 = [],
     captured2 = [],
-    sql1 = 'select * from information_schema.columns order by table_schema, table_name, column_name limit 1;',
-    sql2 = 'select * from information_schema.columns order by table_schema, table_name limit 1;';
+    sql1 =
+      'select * from information_schema.columns order by table_schema, table_name, column_name limit 1;',
+    sql2 =
+      'select * from information_schema.columns order by table_schema, table_name limit 1;';
 
-  const compare1 = await conn.promise().query(
-    sql1
-  );
-  const compare2 = await conn.promise().query(
-    sql2
-  );
+  const compare1 = await conn.promise().query(sql1);
+  const compare2 = await conn.promise().query(sql2);
 
   if (!compare1 || compare1.length < 1) {
     assert.fail('no results for comparison 1');
@@ -25,7 +22,7 @@ const { createConnection } = require('../common.test.cjs');
     assert.fail('no results for comparison 2');
   }
 
-  const stream = conn.query(`${ sql1 }\n${ sql2 }`).stream();
+  const stream = conn.query(`${sql1}\n${sql2}`).stream();
   stream.on('result', (row, datasetIndex) => {
     if (datasetIndex === 0) {
       captured1.push(row);
@@ -38,24 +35,19 @@ const { createConnection } = require('../common.test.cjs');
   // the stream will emit "readable" and if we don't
   // read then 'end' won't be emitted and the
   // test will hang.
-  stream.on("readable", () => {
+  stream.on('readable', () => {
     stream.read();
   });
 
   await new Promise((resolve, reject) => {
-    stream.on('error', e => reject(e));
+    stream.on('error', (e) => reject(e));
     stream.on('end', () => resolve());
   });
 
-  try {
-    assert.equal(captured1.length, 1);
-    assert.equal(captured2.length, 1);
-    assert.deepEqual(captured1[0], compare1[0][0]);
-    assert.deepEqual(captured2[0], compare2[0][0]);
-    process.exit(0);
-  } catch (e) {
-    console.error(e);
-    process.exit(1);
-  }
+  assert.equal(captured1.length, 1);
+  assert.equal(captured2.length, 1);
+  assert.deepEqual(captured1[0], compare1[0][0]);
+  assert.deepEqual(captured2[0], compare2[0][0]);
 
+  process.exit(0);
 })();
