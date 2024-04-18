0004c60286b8c4081e792a4cccaaaf623c66e783
John Lindal <github@newplanetsoftware.com>
2024-04-18 16:06:08 -0700

    fix formatting

--- benchmarks/bench-fake-server.js
+++ benchmarks/bench-fake-server.js
@@ -39,10 +39,7 @@ function benchmarkSelects(n, cb) {
     const end = process.hrtime();
     const diff = common.hrdiff(start, end);
     console.log(
-      ` rows: ${(numSelects * 1e9) / diff}} results/sec, ${(rowsPerQuery *
-        numSelects *
-        1e9) /
-        diff} rows/sec`
+      ` rows: ${(numSelects * 1e9) / diff}} results/sec, ${(rowsPerQuery * numSelects * 1e9) / diff} rows/sec`
     );
     if (n > 1) benchmarkSelects(n - 1, cb);
     else cb();
