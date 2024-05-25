0c9e644e22c6121290815b4b1741b1b554222e11
Andrey Sidorov <andrey.sidorov@gmail.com>
2024-05-25 20:53:31 +0800

    add deno smoke test action

---
+++ test/deno.ts
@@ -0,0 +1,26 @@
+import { createRequire } from 'node:module'
+const require = createRequire(import.meta.url);
+const mysql = require('../index.js');
+
+const connection = mysql.createConnection({
+  host: Deno.env.MYSQL_HOST,
+  port: Deno.env.MYSQL_PORT,
+  username: Deno.env.MYSQL_USER,
+  password: Deno.env.MYSQL_PASSWORD,
+  database: Deno.env.MYSQL_DATABASE,
+});
+
+connection.on('error', (err) => {
+  console.error(err);
+  Deno.exit(1);
+});
+
+connection.on('connect', () => {
+    connection.query('SELECT 1 + 1 AS solution', (err, rows) => {
+    if (err) {
+        console.error(err);
+        Deno.exit(1);
+    }
+    connection.end();
+    });
+});
