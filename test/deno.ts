ec7864599cb8ed773d34a384422f474cc16ea019
Andrey Sidorov <andrey.sidorov@gmail.com>
2024-05-25 22:10:06 +0800

    fix test

--- test/deno.ts
+++ test/deno.ts
@@ -1,22 +1,22 @@
-import { createRequire } from 'node:module';
+import { createRequire } from "https://deno.land/std@0.177.0/node/module.ts";
 const require = createRequire(import.meta.url);
 const mysql = require('../index.js');
 
 const connection = mysql.createConnection({
-  host: Deno.env.MYSQL_HOST,
-  port: Deno.env.MYSQL_PORT,
-  username: Deno.env.MYSQL_USER,
-  password: Deno.env.MYSQL_PASSWORD,
-  database: Deno.env.MYSQL_DATABASE,
+  host: Deno.env.get('MYSQL_HOST'),
+  port: Deno.env.get('MYSQL_PORT'),
+  username: Deno.env.get('MYSQL_USER'),
+  password: Deno.env.get('MYSQL_PASSWORD'),
+  database: Deno.env.get('MYSQL_DATABASE'),
 });
 
-connection.on('error', (err) => {
+connection.on('error', (err: Error) => {
   console.error(err);
   Deno.exit(1);
 });
 
 connection.on('connect', () => {
-  connection.query('SELECT 1 + 1 AS solution', (err) => {
+  connection.query('SELECT 1 + 1 AS solution', (err: Error) => {
     if (err) {
       console.error(err);
       Deno.exit(1);
