d3acac70e7455d4cd75018d98e07f01ed4c9b71e
Andrey Sidorov <andrey.sidorov@gmail.com>
2024-05-27 09:39:08 +1000

    temporarily disable eslint for deno

--- test/deno.ts
+++ test/deno.ts
@@ -1,3 +1,6 @@
+// TODO: fix "[warn] Code style issues found in the above file. Run Prettier to fix." error
+/* eslint-disable */
+
 import { createRequire } from "node:module";
 const require = createRequire(import.meta.url);
 const mysql = require('../index.js');
@@ -5,7 +8,7 @@ const mysql = require('../index.js');
 const connection = mysql.createConnection({
   host: Deno.env.get('MYSQL_HOST'),
   port: Deno.env.get('MYSQL_PORT'),
-  username: Deno.env.get('MYSQL_USER'),
+  user: Deno.env.get('MYSQL_USER'),
   password: Deno.env.get('MYSQL_PASSWORD'),
   database: Deno.env.get('MYSQL_DATABASE'),
 });
