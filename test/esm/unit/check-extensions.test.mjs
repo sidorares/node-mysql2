01c715e443edd9472f36c4c36c70630057f301f6
Weslley Araújo <46850407+wellwelwel@users.noreply.github.com>
2024-07-13 10:30:24 -0300

    chore(deps): upgrade `poku` (#2855)

--- test/esm/unit/check-extensions.test.mjs
+++ test/esm/unit/check-extensions.test.mjs
@@ -1,35 +1,37 @@
 import { EOL } from 'node:os';
-import { listFiles, assert } from 'poku';
+import { listFiles, test, assert } from 'poku';
 
 const invalidFiles = [];
 const message = [
   'Check for invalid file types found in restricted directories',
 ];
 
-const checkExtensions = (
+const checkExtensions = async (
   dirs,
   allowedExtensions,
   ignoreList = /\.DS_Store/,
 ) => {
-  dirs.forEach((dir) => {
-    const files = listFiles(dir, { filter: /\./ });
+  for (const dir of dirs) {
+    const files = await listFiles(dir, { filter: /\./ });
 
-    files.forEach((file) => {
+    for (const file of files) {
       if (!ignoreList.test(file) && !allowedExtensions.test(file)) {
         invalidFiles.push(file);
         message.push(`${EOL}${String(allowedExtensions)}`);
         message.push(`- ${file}`);
       }
-    });
-  });
+    }
+  }
 };
 
-checkExtensions(['test/unit', 'test/integration'], /\.test\.cjs$/);
-checkExtensions(['test/esm'], /\.test\.mjs$/);
-checkExtensions(['test/tsc-build'], /(\.test\.ts|tsconfig\.json)$/);
+test(async () => {
+  await checkExtensions(['test/unit', 'test/integration'], /\.test\.cjs$/);
+  await checkExtensions(['test/esm'], /\.test\.mjs$/);
+  await checkExtensions(['test/tsc-build'], /(\.test\.ts|tsconfig\.json)$/);
 
-assert.deepStrictEqual(
-  invalidFiles.length,
-  0,
-  Array.from(new Set(message)).join(EOL),
-);
+  assert.deepStrictEqual(
+    invalidFiles.length,
+    0,
+    Array.from(new Set(message)).join(EOL),
+  );
+});
--- website/test/unit/check-extensions.test.ts
+++ website/test/unit/check-extensions.test.ts
@@ -1,37 +1,39 @@
 import { EOL } from 'node:os';
-import { listFiles, assert } from 'poku';
+import { listFiles, test, assert } from 'poku';
 
 const invalidFiles: string[] = [];
 const message = [
   'Check for invalid file types found in restricted directories',
 ];
 
-const checkExtensions = (
+const checkExtensions = async (
   dirs: string[],
   allowedExtensions: RegExp,
   ignoreList: RegExp = /\.DS_Store/
 ) => {
-  dirs.forEach((dir) => {
-    const files = listFiles(dir, { filter: /\./ });
+  for (const dir of dirs) {
+    const files = await listFiles(dir, { filter: /\./ });
 
-    files.forEach((file) => {
+    for (const file of files) {
       if (!ignoreList.test(file) && !allowedExtensions.test(file)) {
         invalidFiles.push(file);
         message.push(`${EOL}${String(allowedExtensions)}`);
         message.push(`- ${file}`);
       }
-    });
-  });
+    }
+  }
 };
 
-checkExtensions(['docs', 'i18n'], /\.(mdx|json)$/);
-checkExtensions(['helpers', 'plugins'], /\.ts$/);
-checkExtensions(['test/unit', 'test/utils'], /\.test\.ts$/);
-checkExtensions(['src/components', 'src/pages'], /\.tsx$/);
-checkExtensions(['src/css'], /\.scss$/);
+test(async () => {
+  await checkExtensions(['docs', 'i18n'], /\.(mdx|json)$/);
+  await checkExtensions(['helpers', 'plugins'], /\.ts$/);
+  await checkExtensions(['test/unit', 'test/utils'], /\.test\.ts$/);
+  await checkExtensions(['src/components', 'src/pages'], /\.tsx$/);
+  await checkExtensions(['src/css'], /\.scss$/);
 
-assert.deepStrictEqual(
-  invalidFiles.length,
-  0,
-  Array.from(new Set(message)).join(EOL)
-);
+  assert.deepStrictEqual(
+    invalidFiles.length,
+    0,
+    Array.from(new Set(message)).join(EOL)
+  );
+});
