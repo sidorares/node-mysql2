fdca5d7ea7fd60a0a6f22fb47c2e4ee14fc0b0a5
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2025-01-31 02:43:32 -0300

    ci: test static parser through all tests and variations

--- test/esm/integration/parsers/json-string.test.mjs
+++ test/esm/integration/parsers/json-string.test.mjs
@@ -1,13 +1,16 @@
-import { test, describe, assert } from 'poku';
+import { skip, test, describe, assert } from 'poku';
 import { createRequire } from 'node:module';
 
 const require = createRequire(import.meta.url);
 const {
   createConnection,
   describeOptions,
+  useStaticParser,
 } = require('../../../common.test.cjs');
 
 describe('JSON String', describeOptions);
+
+if (useStaticParser) skip('Skipping during debugging process');
 
 const connection = createConnection({
   jsonStrings: true,
