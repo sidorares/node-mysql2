9642a1e53469ec2c37f9e27850ae19a0da9871fd
Weslley Araújo <46850407+wellwelwel@users.noreply.github.com>
2025-08-27 09:40:15 -0300

    fix(stream): resume connection when stream errors or is destroyed (#3775)
    * fix(stream): resume connection when stream errors or is destroyed
    
    * fix: remove listeners when stream is destroyed
    
    * ci: remove unreachable logic
    
    * chore: remove stream listener when it is destroyed
    
    * chore: ensure the error is emitted
    
    * chore: TOC
    
    * refactor: use native `close` and auto detroy
    
    * refactor: follow ESLint rules

--- test/integration/connection/test-stream-error-destroy-connection.test.cjs
+++ test/integration/connection/test-stream-error-destroy-connection.test.cjs
@@ -1,45 +1,92 @@
 'use strict';
 
-const process = require('node:process');
-const { test, skip } = require('poku');
+const { assert, describe, test } = require('poku');
 const common = require('../../common.test.cjs');
 
-if (process.env.MYSQL_USE_TLS === '1') skip('Skipping for SSL=1');
-
-test('Ensure stream ends in case of error', async () => {
+describe(async () => {
   const connection = common.createConnection();
 
-  connection.query(
-    [
-      'CREATE TEMPORARY TABLE `items` (',
-      '`id` int(11) NOT NULL AUTO_INCREMENT,',
-      '`text` varchar(255) DEFAULT NULL,',
-      'PRIMARY KEY (`id`)',
-      ') ENGINE=InnoDB DEFAULT CHARSET=utf8',
-    ].join('\n'),
-    (err) => {
-      if (err) {
-        throw err;
+  await test('Ensure stream ends in case of error', async () => {
+    connection.query(
+      [
+        'CREATE TEMPORARY TABLE `items` (',
+        '`id` int(11) NOT NULL AUTO_INCREMENT,',
+        '`text` varchar(255) DEFAULT NULL,',
+        'PRIMARY KEY (`id`)',
+        ') ENGINE=InnoDB DEFAULT CHARSET=utf8',
+      ].join('\n'),
+      (err) => {
+        if (err) {
+          throw err;
+        }
       }
+    );
+
+    for (let i = 0; i < 100; i++) {
+      connection.execute(
+        'INSERT INTO items(text) VALUES(?)',
+        ['test'],
+        (err) => {
+          if (err) {
+            throw err;
+          }
+        }
+      );
     }
-  );
 
-  for (let i = 0; i < 100; i++) {
-    connection.execute('INSERT INTO items(text) VALUES(?)', ['test'], (err) => {
-      if (err) {
-        throw err;
-      }
+    const rows = connection.query('SELECT * FROM items').stream();
+
+    // eslint-disable-next-line no-unused-vars
+    for await (const _ of rows) break; // forces return () -> destroy()
+  });
+
+  await test('end: Ensure stream emits error then close on server-side query error', async () => {
+    let uncaughtExceptionError;
+
+    const stream = connection
+      .query('SELECT invalid_column FROM invalid_table')
+      .stream();
+
+    stream.on('error', (error) => {
+      uncaughtExceptionError = error;
     });
-  }
 
-  const rows = connection.query('SELECT * FROM items').stream();
+    await new Promise((resolve) => stream.on('end', resolve));
 
-  // eslint-disable-next-line no-unused-vars
-  for await (const _ of rows) break;
+    assert(
+      uncaughtExceptionError instanceof Error,
+      'Expected an uncaught exception error'
+    );
 
-  setTimeout(() => {
-    throw new Error('Connection remains open after stream error');
-  }, 1000).unref();
+    assert.equal(
+      uncaughtExceptionError.message,
+      "Table 'test.invalid_table' doesn't exist"
+    );
+  });
+
+  await test('close: Ensure stream emits error then close on server-side query error', async () => {
+    let uncaughtExceptionError;
+
+    const stream = connection
+      .query('SELECT invalid_column FROM invalid_table')
+      .stream();
+
+    stream.on('error', (error) => {
+      uncaughtExceptionError = error;
+    });
+
+    await new Promise((resolve) => stream.on('close', resolve));
+
+    assert(
+      uncaughtExceptionError instanceof Error,
+      'Expected an uncaught exception error'
+    );
+
+    assert.equal(
+      uncaughtExceptionError.message,
+      "Table 'test.invalid_table' doesn't exist"
+    );
+  });
 
   connection.end();
 });
