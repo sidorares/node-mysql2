ba0cb983e8b4123a5431c2b97b1da13cc6bfe3fc
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2026-02-19 05:17:44 -0300

    chore: configure tests

---
+++ poku.config.js
@@ -0,0 +1,24 @@
+const { defineConfig } = require('poku');
+
+const commonConfig = defineConfig({
+  debug: true,
+  reporter: 'compact',
+  deno: {
+    allow: ['read', 'env', 'net', 'sys'],
+  },
+});
+
+const parallel = defineConfig({
+  ...commonConfig,
+  exclude: [/test[\\/]esm[\\/]global/],
+  concurrency: 4,
+});
+
+const sequential = defineConfig({
+  ...commonConfig,
+  concurrency: 1,
+});
+
+const suite = process.env.SUITE === 'global' ? sequential : parallel;
+
+module.exports = suite;
