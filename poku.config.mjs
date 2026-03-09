9d384af18415ad8340f77af7d535c808c7edda70
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2026-03-09 13:38:57 -0300

    debug: let's breaking everything

--- poku.config.mjs
+++ poku.config.mjs
@@ -13,7 +13,7 @@ const parallel = defineConfig({
   ...commonConfig,
   timeout: 30000,
   exclude: [/test[\\/]global/, /test[\\/]tsc-build/],
-  concurrency: 8,
+  concurrency: 0,
 });
 
 const sequential = defineConfig({
