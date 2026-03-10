fc8a4d0255c03b174f82b9b2c10fb5705a37643e
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2026-03-09 22:00:44 -0300

    ci: skip global tests for insufficient privileges

--- poku.config.mjs
+++ poku.config.mjs
@@ -1,6 +1,21 @@
 // @ts-check
 
+import { exit } from 'node:process';
 import { defineConfig } from 'poku';
+import { definePlugin } from 'poku/plugins';
+import { hasPrivileges } from './test/common.test.mts';
+
+const setup = {
+  sequential: definePlugin({
+    name: 'setup',
+    async setup() {
+      if (!(await hasPrivileges())) {
+        console.log('❌ Skipping global tests: insufficient privileges');
+        exit(0);
+      }
+    },
+  }),
+};
 
 const commonConfig = defineConfig({
   reporter: 'compact',
@@ -20,6 +35,7 @@ const sequential = defineConfig({
   ...commonConfig,
   timeout: 60000,
   concurrency: 1,
+  plugins: [setup.sequential],
 });
 
 const suite = process.env.SUITE === 'global' ? sequential : parallel;
